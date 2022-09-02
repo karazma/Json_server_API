1a6f3bf4f832030ba3ac0f27a297eca44e443d06
karazma <kkeyanece2128@gmail.com>
2022-09-03 01:53:28 +0530

    product added

--- db.js
+++ db.js
@@ -2,12 +2,15 @@ const items = require("./data/items");
 const tags = require("./data/tags");
 const brands = require("./data/brands");
 const itemTypes = require("./data/itemTypes");
+const dbTest = require("./data/product.json")
+
 
 module.exports = {
   items,
   tags,
   brands,
-  itemTypes
+  itemTypes,
+  dbTest
 }
 
 
